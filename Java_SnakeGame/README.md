8b69f3b29041a6f216fc95eee3ba7867eeba0600
nicoTC-04 <nicolastrevinoc@hotmail.com>
2023-12-04 10:45:46 -0600

    added java snake game

---
+++ Java_SnakeGame/README.md
@@ -0,0 +1,2 @@
+# Java_SnakeGame
+Java Snake game I made while I was learning java in January 2021
---
+++ Java_SnakeGame/src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public Main(){
+        
+        JFrame frame = new JFrame();
+        Gamepanel gamepanel = new Gamepanel();
+        
+        frame.add(gamepanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("SNKAECODING");
+        
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+    public static void main(String[] args) {
+        
+        new Main();
+    }
+}
